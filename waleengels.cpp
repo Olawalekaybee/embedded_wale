f16c8553ddc058dbea9cc8bef1d618236669e915
Balogun olawale <baloolani@gmail.com>
2022-07-01 11:10:59 +0100

    wale2

---
+++ waleengels.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int engels = 1 ;
+    for (int i = 0; i < 5; i++)
+    {
+        engels +=1;
+        printf("%d\n\t", engels);
+    }
+    
+     return 0;
+}
